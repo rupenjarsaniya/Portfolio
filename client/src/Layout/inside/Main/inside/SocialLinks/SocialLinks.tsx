8932207ffbf835938f3ad356ee3c59a5b4bbb965
rupenjarsaniya <jarsaniyarupen7984@gmail.com>
2023-04-20 18:38:23 -0700

    Sociallinks color plates change

--- client/src/Layout/inside/Main/inside/SocialLinks/SocialLinks.tsx
+++ client/src/Layout/inside/Main/inside/SocialLinks/SocialLinks.tsx
@@ -12,39 +12,53 @@ import websiteIcon from "@/asserts/social/website.png";
 
 const data = [
   {
-    link: "https://www.twitter.com/rupenjarsaniya",
-    icon: twitterIcon,
-    title: "Twitter",
-  },
-  {
-    link: "mailto://rupenjarsaniya7984@gmail.com",
-    icon: gmailIcon,
-    title: "Gmail",
-  },
-  {
     link: "https://www.github.com/rupenjarsaniya",
     icon: githubIcon,
     title: "Github",
+    borderColor: "#9da2ac",
+    background: "#9da2ac40",
   },
   {
     link: "https://www.linkedin.com/in/rupenjarsaniya",
     icon: linkedinIcon,
     title: "Linkedin",
+    borderColor: "#78b4dc",
+    background: "#78b4dc40",
   },
   {
-    link: "https://auth.geeksforgeeks.org/user/rupenjarsaniya/",
-    icon: gfgIcon,
-    title: "Geeksforgeeks",
+    link: "https://www.twitter.com/rupenjarsaniya",
+    icon: twitterIcon,
+    title: "Twitter",
+    borderColor: "#08acf4",
+    background: "#08acf440",
   },
   {
     link: "https://www.medium.com/rupenjarsaniya",
     icon: mediumIcon,
     title: "Medium",
+    borderColor: "#ac9c94",
+    background: "#ac9c9440",
+  },
+  {
+    link: "mailto://rupenjarsaniya7984@gmail.com",
+    icon: gmailIcon,
+    title: "Gmail",
+    borderColor: "#e05c54",
+    background: "#e05c5440",
+  },
+  {
+    link: "https://auth.geeksforgeeks.org/user/rupenjarsaniya/",
+    icon: gfgIcon,
+    title: "Geeksforgeeks",
+    borderColor: "#48a444",
+    background: "#48a44440",
   },
   {
     link: "https://www.rupen.vercel.app",
     icon: websiteIcon,
     title: "Website",
+    borderColor: "#3080f8",
+    background: "#3080f840",
   },
 ];
 
@@ -58,6 +72,11 @@ export const SocialLinks: FC = () => (
           target="_blank"
           className={s.linkWrapper}
           key={index}
+          style={{
+            background: item.background,
+            borderColor: item.borderColor,
+            color: item.borderColor,
+          }}
         >
           <div className={s.imageWrapper}>
             <Image src={item.icon} alt={item.link} quality={100} />
