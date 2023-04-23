308c9b72d5247d3dc4a42566f45f24baf6778824
rupenjarsaniya <jarsaniyarupen7984@gmail.com>
2023-04-23 01:00:39 -0700

    Implement and Design Skills Component

--- client/src/Layout/inside/Main/inside/Skills/Skills.tsx
+++ client/src/Layout/inside/Main/inside/Skills/Skills.tsx
@@ -1,4 +1,31 @@
 import { FC } from "react";
 import s from "./Skills.module.scss";
 
-export const Skills: FC = () => <div>Skills</div>;
+const data = [
+  "Solidity programming language",
+  "Ethereum, Hyperledger Fabric platforms",
+  "Smart contract development",
+  "Cryptography",
+  "Web3.js & Ethereum Development",
+  "Decentralized application (DApp) development",
+  "APIs and SDKs for blockchain development (e.g. Ethereum API, Web3.js, Hyperledger Fabric SDK)",
+  "Data Structure & Algorithms",
+  "Proficiency in Java & JavaScript",
+  "Testing and debugging",
+  "Collaboration and teamwork",
+  "Strong problem-solving and analytical skills",
+  "Experience with agile software development methodologies",
+  "Ability to work collaboratively with cross-functional teams, including designers, project managers, and quality assurance testers",
+  "Strong communication and interpersonal skills",
+  "Ability to write clean, efficient, and maintainable code",
+];
+
+export const Skills: FC = () => (
+  <div className={s.wrap}>
+    <ul className={s.list}>
+      {data.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
+  </div>
+);
